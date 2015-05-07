502433c33b83aee9efac3a9e6b6951cc7546f9ca
rkallepu <rashmika23@gmail.com>
2015-05-07 09:27:05 -0700

    changes

--- angular-services/contacts/contacts.js
+++ angular-services/contacts/contacts.js
@@ -10,11 +10,18 @@ angular.module('myApp.contacts', []).controller('ContactsController', function (
         $scope.contacts = Contact.getAll();
         console.log($scope.contacts);
     };
+    $scope.delContact = function(id){
+        Contact.delete(id, Contact.getAll());
+        //console.log(obj);
+    };
 }).factory('Contact', function () {
     // This is a factory function that will return a service (singleton object)
     //var contacts = {};
-    var contacts = JSON.parse(localStorage.getItem('contacts')) || {};
-    //var contacts = JSON.parse(localStorage.getItem('contacts')) || {}; // a private container to store the contacts array
+    if(localStorage.getItem('contacts')) {
+        var contacts = JSON.parse(localStorage.getItem('contacts'));
+    }else
+        var contacts = {};
+    // a private container to store the contacts array
 
     function uuid() {
         // a v4 compliant way of generating a uuid
@@ -40,13 +47,15 @@ angular.module('myApp.contacts', []).controller('ContactsController', function (
         get: function(id){
             return contacts[id];
         },
-        delete: function(id){
-
+        delete: function(id, obj){
+            console.log(id);
+            console.log(obj.splice(id,1));
+            localStorage.setItem("contacts", JSON.stringify(obj.splice(id, 1)));
         },
         update: function (id, obj){
 
         }
     };
-}).controller('MailController', function ($routeParams) {
-    console.log($routeParams.id);
+}).controller('MailController', function ($routeParams, $scope, Contact) {
+    $scope.contact = Contact.get($routeParams.id);
 });
